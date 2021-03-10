a964b5ef7b97bacf625aeab2e33871f79fda74ca
Bretton Wade <bretton.wade@gmail.com>
2021-03-09 19:12:44 -0500

    main - better blinka install

--- rpi/install/HTS221.bash
+++ rpi/install/HTS221.bash
@@ -1,7 +1,5 @@
 #! /usr/bin/env bash
 
 sudo pip3 install adafruit-python-shell;
-wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py;
-sudo python3 raspi-blinka.py;
-rm -f raspi-blinka.py;
+sudo pip3 install adafruit-blinka;
 sudo pip3 install adafruit-circuitpython-hts221;
--- rpi/install/MCP9808.bash
+++ rpi/install/MCP9808.bash
@@ -1,8 +1,6 @@
 #! /usr/bin/env bash
 
 sudo pip3 install adafruit-python-shell;
-wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py;
-sudo python3 raspi-blinka.py;
-rm -f raspi-blinka.py;
+sudo pip3 install adafruit-blinka;
 sudo pip3 install adafruit-circuitpython-mcp9808;
 
