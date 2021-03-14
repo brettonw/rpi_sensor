5596ae70d98b57e0b94f9650a48cb7656be9a7d4
Bretton Wade <bretton.wade@gmail.com>
2021-03-14 18:54:44 -0400

    main - tweaks

--- rpi/bin/install-i2c.bash
+++ rpi/bin/install-i2c.bash
@@ -3,11 +3,34 @@
 pip3 install adafruit-python-shell;
 pip3 install adafruit-blinka;
 
-# enable i2c
+set_config_var() {
+    lua - "$1" "$2" "$3" <<EOF > "$3.bak"
+    local key=assert(arg[1])
+    local value=assert(arg[2])
+    local fn=assert(arg[3])
+    local file=assert(io.open(fn))
+    local made_change=false
+    for line in file:lines() do
+      if line:match("^#?%s*"..key.."=.*$") then
+        line=key.."="..value
+        made_change=true
+      end
+      print(line)
+    end
+
+    if not made_change then
+      print(key.."="..value)
+    end
+    EOF
+    mv "$3.bak" "$3"
+}
+
+# enable i2c - code based on raspi-config
 SETTING=on
 STATUS=enabled
-set_config_var dtparam=i2c_arm $SETTING $CONFIG &&
-sed /etc/modules -i -e "s/^#[[:space:]]*\(i2c[-_]dev\)/\1/"
+CONFIG=/boot/config.txt
+
+set_config_var dtparam=i2c_arm $SETTING $CONFIG && sed /etc/modules -i -e "s/^#[[:space:]]*\(i2c[-_]dev\)/\1/"
 if ! grep -q "^i2c[-_]dev" /etc/modules; then
 printf "i2c-dev\n" >> /etc/modules
 fi
--- rpi/bin/install.bash
+++ rpi/bin/install.bash
@@ -11,44 +11,46 @@ homePath="/home/$user";
 sensorDir="/var/www/html/sensor";
 sudo rm -rf "$sensorDir";
 if [ ! -d "$sensorDir" ]; then
-  sudo mkdir "$sensorDir";
-  sudo chown $user:$user "$sensorDir";
-  chmod ugo+r "$sensorDir";
-  echo "Created sensor dir ($sensorDir).";
+    sudo mkdir "$sensorDir";
+    sudo chown $user:$user "$sensorDir";
+    chmod ugo+r "$sensorDir";
+    echo "Created sensor dir ($sensorDir).";
 fi
 
 # configure a sensor
 binPath="$homePath/bin";
 configuredSensorFile="$binPath/sensor.py";
 if [ ! -e "$configuredSensorFile" ]; then
-  installPath="$executingDir/../install";
-  sensorPath="$executingDir/../sensors";
+    installPath="$executingDir/../install";
+    sensorPath="$executingDir/../sensors";
 
-  echo "No sensor is configured. The choices are:";
-  find "$sensorPath" -printf '    %P\n' | sed -e "s/.py$//";
-  read -p "Which sensor would you like to use? " sensorName;
+    echo "No sensor is configured. The choices are:";
+    find "$sensorPath" -printf '    %P\n' | sed -e "s/.py$//";
+    read -p "Which sensor would you like to use? " sensorName;
 
-  targetInstall="$installPath/$sensorName.bash";
-  if [ -e "$targetInstall" ]; then
-    $($targetInstall);
-  fi
+    targetInstall="$installPath/$sensorName.bash";
+    if [ -e "$targetInstall" ]; then
+        echo "Installing support for $sensorName";
+        sudo $($targetInstall);
+    fi
 
-  targetSensor="$sensorPath/$sensorName.py";
-  if [ -e "$targetSensor" ]; then
-    ln -s "$targetSensor" "$configuredSensorFile";
-  fi
+    targetSensor="$sensorPath/$sensorName.py";
+    if [ -e "$targetSensor" ]; then
+        echo "Linking $sensorName";
+        ln -s "$targetSensor" "$configuredSensorFile";
+    fi
 fi
 
 if [ -e "$configuredSensorFile" ]; then
-  existing=$(ls -l "$configuredSensorFile" | sed -e "s/^.*-> //");
-  existing=$(basename "$existing" | sed -e "s/.py$//");
-  echo "Configured for sensor ($existing).";
-  # copy the service file to the lib directory and start it
-  serviceName="get-sensor.service";
-  echo "Installing service \"$serviceName\"...";
-  sudo systemctl stop "$serviceName";
-  sudo cp "$executingDir/$serviceName" "/lib/systemd/system/"
-  sudo systemctl enable "$serviceName";
-  sudo systemctl start "$serviceName";
+    existing=$(ls -l "$configuredSensorFile" | sed -e "s/^.*-> //");
+    existing=$(basename "$existing" | sed -e "s/.py$//");
+    echo "Configured for sensor ($existing).";
+    # copy the service file to the lib directory and start it
+    serviceName="get-sensor.service";
+    echo "Installing service \"$serviceName\"...";
+    sudo systemctl stop "$serviceName";
+    sudo cp "$executingDir/$serviceName" "/lib/systemd/system/"
+    sudo systemctl enable "$serviceName";
+    sudo systemctl start "$serviceName";
 fi
 echo "Done.";
