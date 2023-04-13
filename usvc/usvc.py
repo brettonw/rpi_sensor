e8afab14021bffba397182fc10ea7ab5f5654188
Bretton Wade <bretton.wade@gmail.com>
2023-04-13 14:54:52 -0400

    main - new usvc script, dockerfile, and docker-compose file

---
+++ usvc/usvc.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+
+# to test:
+# curl -H "Content-Type: application/json" -X POST -d '{"name": "John", "age": 30}' http://localhost:8081/
+
+from flask import Flask, request, jsonify
+import json
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def process_request():
+    req_data = request.get_json()
+    # do some processing with the request data
+
+    #with open("/usvc/files/test.json", "r") as file:
+    #    data = json.load(file)
+    data = {'message': 'success'}
+
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run()
