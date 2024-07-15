6d6dce7620fe7759c92f6f5f63509c062a125f58
Bretton Wade <bretton.wade@gmail.com>
2024-07-15 14:40:18 -0400

    main - updating PoePlus...

--- rpi/sensors/PoePlus.py
+++ rpi/sensors/PoePlus.py
@@ -5,10 +5,18 @@ from statistics import mean, stdev
 from time import sleep
 
 
-def get_value(command: str, value_on_error: int | float = None) -> int | float | None:
+def get_raw_value(command: str) -> str | None:
     # run the command and capture the output
     result = subprocess.run(command, capture_output=True, text=True, shell=True)
-    return float(result.stdout.strip()) if result.returncode == 0 else value_on_error
+    return result.stdout.strip() if result.returncode == 0 else None
+
+def get_int_value(command: str, value_on_error: int | None = None) -> int | None:
+    result = get_raw_value(command)
+    return int(result) if result is not None else value_on_error
+
+def get_float_value(command: str, value_on_error: int | None = None) -> int | None:
+    result = get_raw_value(command)
+    return float(result) if result is not None else value_on_error
 
 # Example usage
 #command_output = execute_command("echo Hello, World!")
@@ -16,16 +24,16 @@ def get_value(command: str, value_on_error: int | float = None) -> int | float |
 
 
 # get the current fan speed
-fan_speed: int = get_value("cat /sys/class/thermal/cooling_device0/cur_state", -1)
+fan_speed = get_int_value("cat /sys/class/thermal/cooling_device0/cur_state", -1)
 
 # get 5 samples of the µA measurement, converting to A
 samples = []
 def get_sample():
-    sample = get_value("cat /sys/devices/platform/rpi-poe-power-supply/power_supply/rpi-poe/current_now") / 1000000
+    sample = get_float_value("cat /sys/devices/platform/rpi-poe-power-supply/power_supply/rpi-poe/current_now")
     if sample is not None:
-        samples.append(sample)
+        samples.append(sample / 1000000.0)
 get_sample()
-for i in range(5):
+for i in range(4):
     sleep(0.1)
     get_sample()
 
